52c1fddff06f8c9b7c7526a750963b4fb353befd
Jay Lee <jay.lee@lunit.io>
2023-06-06 17:32:25 +0900

    Added more notes on ownership

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rust-practice
-Simple repository for getting my feet wet with rust before I work on some side projects
+
+Simple repository for getting my feet wet with rust before I work on some side projects.
+
+## Credits
+
+These notes were made while watching the `rust-programming-master-class-from-beginner-to-expert` 
+lectures in udemy, so special credit to the creator of the course. Thank you!
--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,28 @@ fn main() {
     println!("Square output: {}", variable_scope::square(2));
     variable_scope::strings_heap();
     let yee = 987;
+    // Even though we updated the 
+    // value inside of stack_fn, because it is a primitive,
+    // it is stored on the stack. Thus, once it goes out of scope,
+    // the local instance is deleted.
+    // Only objects in the stack are maintained until the owner 
+    // goes out of scope.
     ownership::stack_fn(yee);
     println!("Yee: {yee}");
+
+    let mut numbers = vec![1, 2, 3, 4];
+    
+    // Need to pass by reference
+    // &: Ownership will be maintained by "vector" 
+    // inside stack_fn_vec
+    // mut: Enable updating the vector
+    // "&mut" - Pass in a mutable reference.
+    ownership::stack_fn_vec(&mut numbers);
+    println!("numbers: {:?}", numbers);
+
+    let mut vec1 = vec!{1, 2, 3};
+    // Correct syntax for mutable reference
+    let ref1 = &mut vec1;
+    ref1.push(4);
+    println!("vec1: {:?}", vec1);
 }
