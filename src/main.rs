3ebfab1d409307ed6b6b3ae3d3d9e86c3b9f64a1
Jay Lee <jay.lee@lunit.io>
2024-01-20 16:57:56 +0900

    Add trait sample

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,23 @@
-use std::collections::HashMap;
-mod dummy_projects;
-
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut map: HashMap<i32, usize>= HashMap::with_capacity(nums.len());
-    for (i, &num) in nums.iter().enumerate() {
-        match map.get(&(target - num)) {
-            Some(old_value_index) => return vec![*old_value_index as i32, i as i32],
-            None => map.insert(num, i),
-        };
-    }
-    unreachable!()
+struct Square {
+    width: f32,
 }
 
-struct Person {
-    name: String,
-    age: u8,
-    country: String,
+struct Rectangle {
+    width: f32,
+    height: f32,
 }
 
-trait GeneralInfo {
-    fn info(&self) -> (&str, u8);
-    fn country(&self) -> &str;
-}
+trait Shape {
+    fn area(&self) -> f32;
+} 
 
-impl GeneralInfo for Person {
-    fn info(&self) -> (&str, u8) {
-        (&self.name, self.age)
-    }
-
-    fn country(&self) -> &str {
-        &self.country
+impl Shape for  Rectangle {
+    fn area(&self) -> f32 {
+        self.width * self.height
     }
 }
 
-pub fn square<T>(x: T) -> T 
-    where T: std::ops::Mul<Output = T> + Copy{
-    x * x
-}
-
-struct Point<T, U> {
-    x: T,
-    y: U
-}
-
-impl <T, U> Point <T, U> {
-    fn new(x: T, y: U) -> Self {
-        Point { x, y }
-    }
-}
-
-impl Point<i32, i32> {
-    fn print(&self) {
-        println!("The values of the coordinates are: {} and {}", self.x, self.y);
-    }
-}
 
 fn main() {
-   
+
 }
